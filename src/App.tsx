c7a8751c45a6e70c005a889aa4555c4109157c5f
Howling Techie <howling.techie@gmail.com>
2023-11-24 20:46:41 +0000

    Added user log in system

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import {AuthProvider} from './context/AuthContext';
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Navbar from "./components/Navbar.tsx";
+import Login from "./pages/Login.tsx";
+import Profile from "./pages/Profile.tsx";
 
 
 const App = () => {
     return (
         <Router>
             <AuthProvider>
-                <div className="bg-gray-900 text-white min-h-screen">
+                <div className="bg-background text-dark-text min-h-screen">
                     <Navbar/>
                     <Routes>
                         <Route path="/auth/discord/callback">
@@ -17,6 +19,8 @@ const App = () => {
                         </Route>
                         <Route path="/" element={<Home/>}/>
                         <Route path="/about" element={<About/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
                     </Routes>
                 </div>
             </AuthProvider>
