f3ceb90595a93d0b3c93a9a303c97bb52b1aaf6d
Howling Techie <howling.techie@gmail.com>
2023-11-27 14:26:55 +0000

    Added basic API service

---
+++ src/services/API.ts
@@ -0,0 +1,64 @@
+import axios, {AxiosRequestConfig} from 'axios';
+
+const baseURL = "http://localhost:5000/api";
+
+const axiosInstance = axios.create({
+    baseURL,
+});
+
+// Function to set the access token in the headers
+const setAccessToken = (token: string): AxiosRequestConfig => ({
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
+});
+
+// Function to handle API requests
+const makeRequest = async (config: AxiosRequestConfig) => {
+    try {
+        const response = await axiosInstance(config);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw error.response ? error.response.data : error.message;
+        }
+    }
+};
+
+export const getServer = async (serverId: number, accessToken?: string) => {
+    const config: AxiosRequestConfig = accessToken
+        ? setAccessToken(accessToken)
+        : {};
+
+    return makeRequest({
+        method: 'get',
+        url: `/servers/${serverId}`,
+        ...config,
+    });
+};
+export const getServers = async (accessToken?: string) => {
+    const config: AxiosRequestConfig = accessToken
+        ? setAccessToken(accessToken)
+        : {};
+
+    return makeRequest({
+        method: 'get',
+        url: `/servers`,
+        ...config,
+    });
+};
+
+export const getServerUsers = async (
+    serverId: number,
+    accessToken?: string
+) => {
+    const config: AxiosRequestConfig = accessToken
+        ? setAccessToken(accessToken)
+        : {};
+
+    return makeRequest({
+        method: 'get',
+        url: `/servers/${serverId}/users`,
+        ...config,
+    });
+};
