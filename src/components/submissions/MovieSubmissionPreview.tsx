8c89c64ec110860f56653f27fa17a9fbd1f4be87
Howling Techie <howling.techie@gmail.com>
2023-11-27 20:16:24 +0000

    Added movie info page

---
+++ src/components/submissions/MovieSubmissionPreview.tsx
@@ -0,0 +1,60 @@
+import {Link} from "react-router-dom";
+import Submission from "../../interfaces/Submission.ts";
+
+interface SubmissionProps {
+    submission: Submission
+}
+
+const MovieSubmissionPreview = ({submission}: SubmissionProps) => {
+    const {
+        submission_id,
+        time_submitted,
+        title,
+        description,
+        status,
+        first_appearance,
+        last_appearance,
+        user,
+    } = submission;
+
+    return (
+        <div className="mx-auto mt-8 w-full p-4 border shadow-2xl rounded-xl">
+            <div className="flex items-center justify-between">
+                <h2 className="text-2xl font-semibold">{title}</h2>
+                <p className="text-gray-500">{new Date(time_submitted).toLocaleDateString()}</p>
+            </div>
+            <p className="text-gray-600">{description}</p>
+            <div className="mt-4">
+                <p>
+                    <span className="font-semibold">Status:</span> {status}
+                </p>
+                {first_appearance && <p>
+                    <span className="font-semibold">First Appearance:</span>{' '}
+                    {new Date(first_appearance).toLocaleDateString()}
+                </p>}
+                {last_appearance && <p>
+                    <span className="font-semibold">Last Appearance:</span>{' '}
+                    {new Date(last_appearance).toLocaleDateString()}
+                </p>}
+            </div>
+            <div className="mt-4">
+                <h3 className="text-xl font-semibold">Submitted by:</h3>
+                <div className="flex items-center py-2">
+                    <img
+                        src={"https://cdn.discordapp.com/avatars/" + user.user_id + "/" + user.avatar + (user.avatar.startsWith("a_") ? ".gif" : ".png")}
+                        alt={user.username} className="w-10 h-10 rounded-full mr-2"/>
+                    <div>
+                        <p className="text-gray-900">{user.username}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="my-2">
+                <Link to={`/submissions/${submission_id}`} className="bg-blue-500 text-white px-4 py-2 rounded">
+                    View Submission
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default MovieSubmissionPreview;
--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,5 +1,95 @@
-export const Movie = () => {
+import Movie from "../interfaces/Movie.ts";
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getMovie, getMovieSubmissions} from "../services/API.ts";
+import MovieSubmissionPreview from "../components/submissions/MovieSubmissionPreview.tsx";
+import Submission from "../interfaces/Submission.ts";
+
+const MoviePage = () => {
+    const {movie_id} = useParams();
+    const [movie, setMovie] = useState<null | Movie>(null);
+    const [submissions, setSubmissions] = useState<null | Submission[]>(null);
+
+    useEffect(() => {
+        const getMovieById = async () => {
+            if (movie_id) {
+                const {movie: movieResult} = await getMovie(+movie_id);
+                setMovie(movieResult);
+                const {submissions: submissionResults} = await getMovieSubmissions(+movie_id);
+                setSubmissions(submissionResults);
+            }
+        }
+        if (movie_id)
+            getMovieById()
+    }, [movie_id]);
+
+    const formattedDuration = (duration: number) => {
+        const hours = Math.floor(duration / 60);
+        const minutes = duration % 60;
+        return `${hours}h ${minutes}min`;
+    }
+
     return (
-        <></>
+        <div>{movie && <div className="flex justify-center">
+            <div className="flex-row flex w-full mt-8 max-w-7xl px-4 ">
+                <div className="flex justify-center grow overflow-clip rounded-2xl max-w-[192px] h-72 ">
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster}`} alt={movie.title}
+                         className="object-cover"/>
+                </div>
+                <div className="flex flex-col ml-4">
+                    <div className="mt-4">
+                        <h2 className="text-2xl font-semibold">{movie.title}</h2>
+                        <p className="text-gray-600">
+                            Release Date: {new Date(movie.release_date).toLocaleDateString()}
+                        </p>
+                        <p className="text-gray-600">Duration: {formattedDuration(movie.duration)}</p>
+                        <p className="mt-2">{movie.description || 'No description available.'}</p>
+                    </div>
+                    <div className="mt-4 flex space-x-4">
+                        <a
+                            href={`https://www.themoviedb.org/movie/${movie.movie_id}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="bg-blue-500 text-white px-4 py-2 rounded"
+                        >
+                            View on TMDB
+                        </a>
+                        {movie.letterboxd_url && (
+                            <a
+                                href={`https://letterboxd.com/film/${movie.letterboxd_url}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="bg-gray-800 text-white px-4 py-2 rounded"
+                            >
+                                View on Letterboxd
+                            </a>
+                        )}
+                        {movie.imdb_id && (
+                            <a
+                                href={`https://www.imdb.com/title/${movie.imdb_id}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="bg-yellow-500 text-black px-4 py-2 rounded"
+                            >
+                                View on IMDb
+                            </a>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+        }
+            {submissions &&
+                <div className="flex justify-center pb-8">
+                    <div className="flex-col flex w-full mt-8 max-w-7xl px-4">
+                        <div className="text-xl font-bold">Submission History</div>
+                        {submissions.map((submission) => {
+                            return <MovieSubmissionPreview submission={submission}/>
+                        })}
+                    </div>
+                </div>}
+        </div>
     );
 };
+
+export default MoviePage;
