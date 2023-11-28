a7f477a134c3d416593cf63e952ec7689af2aec3
Howling Techie <howling.techie@gmail.com>
2023-11-28 17:01:28 +0000

    Removed custom styling from dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,14 +56,14 @@ const Navbar = () => {
                             leaveTo="transform opacity-0 scale-95"
                         >
                             <Menu.Items
-                                className="absolute right-0 mt-2 w-40 origin-top-right divide-y rounded-md bg-surface shadow-lg ring-1 ring-black/5 focus:outline-none">
+                                className="absolute right-0 mt-2 w-40 origin-top-right divide-y rounded-md bg-gray-950 shadow-lg ring-1 ring-black/5 focus:outline-none">
                                 <div className="px-1 py-1 ">
                                     <Menu.Item>
                                         {({active}) => (
                                             <button onClick={() => navigate("/profile")}
                                                     className={`${
-                                                        active ? 'bg-primary' : ''
-                                                    } group flex text-primary-text w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                        active ? 'bg-gray-400' : ''
+                                                    } group flex text-white w-full items-center rounded-md px-2 py-2 text-sm`}
                                             >Profile
                                             </button>
                                         )}
@@ -72,8 +72,8 @@ const Navbar = () => {
                                         {({active}) => (
                                             <button onClick={() => navigate("/signout")}
                                                     className={`${
-                                                        active ? 'bg-primary' : ''
-                                                    } group flex text-primary-text w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                        active ? 'bg-gray-400' : ''
+                                                    } group flex text-white w-full items-center rounded-md px-2 py-2 text-sm`}
                                             >
                                                 Sign Out
                                             </button>
