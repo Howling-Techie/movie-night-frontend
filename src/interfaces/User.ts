acd2df0e7f2d16e0adbc8a7c8c29253f2b910471
Howling Techie <howling.techie@gmail.com>
2023-11-28 19:03:49 +0000

    Some users may not have avatars or banners

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -2,8 +2,8 @@ export default interface User {
     "user_id": string,
     "username": string,
     "display_name": string,
-    "avatar": string,
-    "banner": string,
+    "avatar": string | null,
+    "banner": string | null,
     "banner_color": string,
     "description": string
 }
