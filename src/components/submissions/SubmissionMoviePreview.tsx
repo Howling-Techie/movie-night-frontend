778cc5fc14c25b05d42087fe4d50789c865381a3
Howling Techie <howling.techie@gmail.com>
2023-11-28 19:04:28 +0000

    Made submission movie previews work better on smaller screens

--- src/components/submissions/SubmissionMoviePreview.tsx
+++ src/components/submissions/SubmissionMoviePreview.tsx
@@ -7,11 +7,13 @@ interface MovieProp {
 const SubmissionMoviePreview = ({movie}: MovieProp) => {
     const {movie_info} = movie;
     const poster = `https://image.tmdb.org/t/p/w500${movie.poster ?? movie_info.poster}`;
-    //const image = `https://image.tmdb.org/t/p/w500${movie.image ?? movie_info.image}`;
+    const image = `https://image.tmdb.org/t/p/w500${movie.image ?? movie_info.image}`;
     return (
-        <div className="bg-gray-100 shadow-md rounded-md flex flex-row m-4">
-            {poster && <img src={poster} alt={movie_info.title} className="w-32 mr-4 object-cover rounded"/>}
-            <div className="flex flex-col mt-2 p-2 ">
+        <div className="bg-gray-100 shadow-md rounded-md flex flex-col md:flex-row overflow-clip">
+            {poster &&
+                <img src={poster} alt={movie_info.title} className="w-48 object-cover hidden md:inline"/>}
+            {image && <img src={image} alt={movie_info.title} className="h-48 object-cover inline md:hidden"/>}
+            <div className="flex flex-col mx-4 mt-2 p-2 ">
                 <h4 className="text-lg font-semibold">{movie_info.title}</h4>
                 <span className="text-gray-500">{new Date(movie_info.release_date).toLocaleDateString()}</span>
                 <div className="flex mt-2">
