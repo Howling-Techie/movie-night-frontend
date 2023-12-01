b4cd478dd877c43db8551ed4e061c720d3be62cb
Howling Techie <howling.techie@gmail.com>
2023-12-01 18:00:15 +0000

    Fixed event return type to match API

--- src/interfaces/Event.ts
+++ src/interfaces/Event.ts
@@ -6,8 +6,8 @@ export default interface Event {
     description: string | null,
     time_created: Date,
     start_time: null | Date,
-    voting_open_time: null | Date,
-    voting_close_time: null | Date,
+    voting_opening_time: null | Date,
+    voting_closing_time: null | Date,
     server: Server,
     tag: null | { "name": string, "description": string, "icon": string }
 }
