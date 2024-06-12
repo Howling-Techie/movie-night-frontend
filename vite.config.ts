0ca1c699162a7af38bc5fd8af8ee95ae01717b95
Howling Techie <howling.techie@gmail.com>
2024-06-12 16:46:52 +0100

    Removed custom hmr code

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    hmr: {
-      host: 'hechie.com',
-      port: 5101,
-      protocol: 'wss',
-    }
-  },
-  plugins: [react()],
-  base: "/movies",
-})
+    server: {
+        host: true,
+        // hmr: {
+        //   host: 'hechie.com',
+        //   port: 5101,
+        //   protocol: 'wss',
+        // }
+    },
+    plugins: [react()],
+    base: "/movies",
+});
